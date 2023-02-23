ae548927bb7f32a9bc8369599e1ab9d76c3b79f2
lethinh2003 <muradvn2003@gmail.com>
2023-02-23 10:45:34 +0700

    Revert to 447b699b7

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   optimizeFonts: false,
   env: {
     ENDPOINT_SERVER: process.env.ENDPOINT_SERVER,
